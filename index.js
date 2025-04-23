1369deb577abb74614b65a50c232f0e70704389e
Joel Eguiza <joeleg96@gmail.com>
2025-04-23 14:17:09 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var clickedButtonSaved = [];
+
+$(".rating-btn").on("click", function(event) {
+    var clickedButtonSaved = [];
+    var clickedButton = (event.target.id);
+    clickedButtonSaved.push(clickedButton);
+     
+    $(".rating-btn").removeClass("chosen");
+    $(this).addClass("chosen");
+    displayRating(clickedButtonSaved);
+})
+
+function displayRating(numberChosen) {
+    console.log("debug:" + numberChosen);
+    switch (numberChosen) {
+        case "one":
+            $(".rating-selected").text("You selected 1 out of 5");
+        break;
+
+        case "two":
+            $(".rating-selected").text("You selected  2 out of 5");
+        break;
+
+        case "three":
+            $(".rating-selected").text("You selected 3 out of 5");
+        break;
+
+        case "four":
+            $(".rating-selected").text("You selected 4 out of 5");
+        break;
+
+        case "five":
+            $(".rating-selected").text("You selected 5 out of 5");
+        break;
+    
+        default:
+            console.log("hello" + numberChosen);
+            break;
+    }
+}
+
+$(".submit-btn").on("click", function() {
+    $(".rating-container").addClass("hidden");
+    $(".thank-you-container").removeClass("hidden");
+});
